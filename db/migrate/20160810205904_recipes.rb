b88684d3802c777f203934fd3c211a27ffa38512
ozPop <ozziepopovas@gmail.com>
2016-08-10 17:03:45 -0400

    update recipes migration, migrate

--- db/migrate/20160810205904_recipes.rb
+++ db/migrate/20160810205904_recipes.rb
@@ -1,4 +1,9 @@
 class Recipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |col|
+      col.string :name
+      col.text :ingredients
+      col.time :cook_time
+    end
   end
 end
