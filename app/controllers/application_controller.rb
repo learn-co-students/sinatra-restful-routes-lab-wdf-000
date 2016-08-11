93cc72cfe41457c7e00ab89d29fb7f92eb75267d
ozPop <ozziepopovas@gmail.com>
2016-08-11 08:13:48 -0400

    add routes to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,43 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
 
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :show
+  end
 
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    @delete_message = "#{@recipe[:name]} was deleted"
+    @recipe = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(params[:recipe])
+    @recipe.save
+    erb :show
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params[:recipe])
+    redirect 'recipes'
+  end
 
 
 end
