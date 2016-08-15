3ed3c708655f76c912f761c618a3163f408dba75
fidelscodes <fidelscodes@gmail.com>
2016-08-15 13:29:00 -0400

    Add container class to layout and create view/action for recipes/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  # Should display all recipes
   get '/' do
     erb :index
   end
+
+  # Display form for creating a new recipe
+  get '/recipes/new' do
+    erb :new
+  end
+
 end
