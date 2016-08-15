009aa219dcc74ab369679eb741bb5353de299ef7
fidelscodes <fidelscodes@gmail.com>
2016-08-15 15:02:24 -0400

    Add show view and corresponding actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
 
   # Should display all recipes
   get '/' do
-    erb :index
+    redirect to '/recipes'
   end
 
   get '/recipes' do
@@ -26,4 +26,66 @@ class ApplicationController < Sinatra::Base
     redirect to 'recipes/:id'
   end
 
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  # Delete action
+  delete '/recipes/:id/delete' do
+    binding.pry
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.delete
+    redirect to '/'
+  end
+
 end
+
+# class ApplicationController < Sinatra::Base
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+#
+#   get '/' do
+#    # erb :index
+#     redirect '/recipes'
+#   end
+#
+#   get '/recipes' do
+#     @recipes = Recipe.all
+#     erb :index
+#   end
+#
+#   get '/recipes/new' do
+#     erb :new
+#   end
+#
+#   get '/recipes/:id' do
+#     @recipe = Recipe.find(params[:id])
+#     erb :show
+#   end
+#
+#   post '/recipes' do
+#     recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+#     redirect "/recipes/#{recipe.id}"
+#   end
+#
+#   get '/recipes/:id/edit' do
+#     @recipe = Recipe.find(params[:id])
+#     erb :edit
+#   end
+#
+#   patch '/recipes/:id/edit' do
+#     recipe = Recipe.find(params[:id])
+#     recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+#     redirect "/recipes/#{recipe.id}"
+#   end
+#
+#   delete '/recipes/:id/delete' do
+#     recipe = Recipe.find(params[:id])
+#     recipe.delete
+#     redirect '/'
+#   end
+# end
--- config.ru
+++ config.ru
@@ -4,4 +4,6 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+# Don't forget about Rack::MethoOverride!
+use Rack::MethodOverride
 run ApplicationController
