959fa2908cf67084e69cc212dd7a6bac4a2cdec5
fidelscodes <fidelscodes@gmail.com>
2016-08-15 14:02:31 -0400

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,21 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
   # Display form for creating a new recipe
   get '/recipes/new' do
     erb :new
+  end
+
+  # Create new recipe with info from /recipes/new page
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.save
+    redirect to 'recipes/:id'
   end
 
 end
