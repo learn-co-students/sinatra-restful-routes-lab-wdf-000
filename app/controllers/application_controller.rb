13b42f4a50430a9f24bc1fbc778cd87e3cadd6cb
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-08-22 17:40:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,53 @@
+require './config/environment'
+require './app/models/recipe'
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  post '/recipes' do
+    # create and save
+    # redirect to show
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do #load edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do # edit action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    Recipe.delete(params[:id])
+    redirect to "/recipes"
+  end
+
 end
